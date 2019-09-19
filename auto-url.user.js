5aff7a2a5da5b12cae39925daf402002e11b843d
Juan Fernando <jfdelarosa@outlook.com>
2019-09-19 11:04:32 -0500

    Update auto-url.user.js
    - Nueva forma de validar los links
    - El enlace se puede pegar en cualquier parte
    - Se permite el uso de ctrl+z

--- auto-url.user.js
+++ auto-url.user.js
@@ -13,35 +13,50 @@
   "use strict";
   GM_log("Created by http://jfdelarosa.github.io/");
   function isValidURL(str) {
-    var pattern = new RegExp(
-      "^(https?:\\/\\/)?" +
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
-        "((\\d{1,3}\\.){3}\\d{1,3}))" +
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
-        "(\\?[;&a-z\\d%_.~+=-]*)?" +
-        "(\\#[-a-z\\d_]*)?$",
-      "i"
-    );
-    return !!pattern.test(str);
+    var a = document.createElement('a');
+    a.href = str;
+    return (a.host && a.host != window.location.host);
+
+    // var pattern = new RegExp(
+    //   "^(https?:\\/\\/)?" +
+    //     "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+    //     "((\\d{1,3}\\.){3}\\d{1,3}))" +
+    //     "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+    //     "(\\?[;&a-z\\d%_.~+=-]*)?" +
+    //     "(\\#[-a-z\\d_]*)?$",
+    //   "i"
+    // );
+    // return !!pattern.test(str);
+  }
+
+  function isTextarea(e){
+    var target = e.target ? e.target : e.srcElement;
+    return {
+      isValid: target && target.nodeName.toLowerCase() == "textarea",
+      target: target
+    }
+  }
+
+  function insertText(textarea, text) {
+    if (!document.execCommand('insertText', false, text)) {
+      textarea.setRangeText(text);
+    }
   }
 
   function addEventListeners() {
     function onLoad() {
-      document.body.addEventListener("paste", function(event) {
-        var target = event.target ? event.target : event.srcElement;
-        if (target && target.nodeName.toLowerCase() == "textarea") {
-          var link = event.clipboardData.getData("text/plain");
-          link = link.replace(/\s+/g, '');
-          var isURL = isValidURL(link);
-          if (isURL) {
-            event.preventDefault();
-            target.value += "[" + link + "](" + link + ")";
+      document.body.addEventListener("paste", function(e) {
+        var el = isTextarea(e);
+        if (el.isValid) {
+          var link = e.clipboardData.getData("text/plain");
+          if (isValidURL(link)) {
+            insertText(el.target, "[" + link + "](" + link + ")")
+            e.preventDefault();
           }
         }
       });
     }
     unsafeWindow.addEventListener("load", onLoad, false);
   }
-
   document.addEventListener("DOMContentLoaded", addEventListeners, false);
 })();
