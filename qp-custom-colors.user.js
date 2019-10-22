a7618117c1a0489fb4f7aae677b488b2a88b9010
Fernando <fernando@mailvibes.io>
2019-10-22 16:56:56 -0500

    Se incluyen mas tipos de gráficos

--- qp-custom-colors.user.js
+++ qp-custom-colors.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QuestionPro Custom colors on Dashboard
 // @namespace    http://jfdelariosa.github.io/
-// @version      0.1
+// @version      0.2
 // @run-at       document-end
 // @description  Changes the default colors on the QuestionPro's dashboard
 // @author       jfdelarosa
@@ -12,28 +12,60 @@
 (function() {
   "use strict";
   GM_log("Created by http://jfdelarosa.github.io/");
-  let colors = ["red", "blue", "green", "yellow"];
-  let defaultColor = "#7D7D7D";
-
+  function getColor(index) {
+    let colors = [
+      "#1abc9c",
+      "#3498db",
+      "#9b59b6",
+      "#f1c40f",
+      "#e67e22",
+      "#e74c3c",
+      "#95a5a6",
+      "#34495e",
+      "#16a085",
+      "#2980b9",
+      "#8e44ad",
+      "#f39c12",
+      "#d35400",
+      "#c0392b",
+      "#2c3e50",
+      "#7f8c8d"
+    ];
+    return colors[index % colors.length];
+  }
   function addEventListeners() {
     function onLoad() {
       document.querySelectorAll(".highcharts-series").forEach(item => {
-        item.querySelectorAll("path").forEach((i, j) => {
-          let color = defaultColor;
-          if (j + 1 <= colors.length) {
-            color = colors[j];
+        item.querySelectorAll("path, rect").forEach((i, j) => {
+          let color = getColor(j);
+          if (
+            i.getAttribute("fill") !== "none" &&
+            i.getAttribute("fill") !== "rgb(125,125,125)" &&
+            i.getAttribute("fill") !== "#7D7D7D"
+          ) {
+            i.setAttribute("fill", color);
           }
-          i.setAttribute("fill", color);
+          if (
+            i.getAttribute("stroke") !== "rgba(192,192,192,0.0001)" &&
+            i.getAttribute("stroke") !== "silver"
+          ) {
+            i.setAttribute("stroke", color);
+          }
+        });
+      });
+      document.querySelectorAll(".highcharts-markers").forEach((item, j) => {
+        item.querySelectorAll("path").forEach(i => {
+          let color = getColor(j);
+          if (i.getAttribute("fill") !== "#FFFFFF") {
+            i.setAttribute("fill", color);
+          }
         });
       });
       document.querySelectorAll(".dashboard-table-data").forEach(table => {
         table
           .querySelectorAll('td[height="14"]:first-child')
           .forEach((item, index) => {
-            let color = defaultColor;
-            if (index + 1 <= colors.length) {
-              color = colors[index];
-            }
+            let color = getColor(index);
             item.style.backgroundColor = color;
           });
       });
